956e72765b26b4ed897c0d7f28380919abe072b7
Mazhar <mi.sabbir420@gmail.com>
2025-12-11 17:58:28 +0600

    Server Simple Setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req,res)=>{
+    res.send('Connected !')
+})
+
+app.listen(port, ()=>{
+    console.log(`From Port-${port}`)
+})
